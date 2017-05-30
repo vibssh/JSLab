9254d69090714d0559eafad8716e45f18146c547
ljacobs <leo@the-earthworks.com>
2017-05-30 21:36:21 +0100

    Revealing Pattern Module with Component Loader POC

--- js/_ModuleOne.js
+++ js/_ModuleOne.js
@@ -1,38 +1,66 @@
-// (function(){
-//     'use strict';
-//     var ModuleOne = function(element){
-//         var $el = $(element).find('p');
-//         console.info($($el).text());
-//     };
+//(function(){
+//    'use strict';
+//    var ModuleOne = {
+//        settings: {
+//
+//        },
+//
+//        init: function(element){
+//            ModuleOne.settings.$el = $(element).find('p');
+//            ModuleOne.settings.$btn = $(element).find('#button');
+//            ModuleOne.bindUIActions();
+//        },
+//
+//        bindUIActions: function(){
+//            ModuleOne.settings.$btn.on('click', function(e){
+//                e.preventDefault();
+//                ModuleOne.changeColor();
+//            });
+//            // console.info($(ModuleOne.settings.$el).html());
+//        },
+//
+//        changeColor: function (){
+//            ModuleOne.settings.$el.css('color', 'red');
+//        }
+//    };
+//    ComponentLoader.ModuleOne = ModuleOne.init;
+//}());
 
-//     ComponentLoader.ModuleOne = ModuleOne;
-// }());
+var Module = function(){
+    //Private Stuff
+    var settings = {
 
+    },
 
-(function(){
-    'use strict';
-    var ModuleOne = {
-        settings: {
-            
-        },
+    init = function(element){
+       settings.$el = $(element).find('p');
+       settings.$btn = $(element).find('#button');
+        console.info(settings.$btn);
+       bindUIActions();
+    },
 
-        init: function(element){
-            ModuleOne.settings.$el = $(element).find('p');
-            ModuleOne.settings.$btn = $(element).find('#button');
-            ModuleOne.bindUIActions();
-        },
+    bindUIActions = function(){
+        settings.$btn.on('click', function(e){
+           e.preventDefault();
+           changeColor();
+        });
+    },
 
-        bindUIActions: function(){
-            ModuleOne.settings.$btn.on('click', function(e){
-                e.preventDefault();
-                ModuleOne.changeColor();
-            });
-            // console.info($(ModuleOne.settings.$el).html());
-        },
-
-        changeColor: function (){
-            ModuleOne.settings.$el.css('color', 'red');
-        }
+    changeColor = function(){
+      settings.$el.css('color', 'red');
     };
-    ComponentLoader.ModuleOne = ModuleOne.init;
-}());
+
+
+    //Public API
+    return {
+       ModuleOne : init
+    };
+
+};
+
+$(document).ready(function(){
+   var m = Module();
+
+    ComponentLoader.ModuleOne = m.ModuleOne;
+
+});
