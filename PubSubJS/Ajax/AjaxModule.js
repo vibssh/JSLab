3e7c0344c62b49e2106c60604d1a88b8093f26f4
Leo Jacobs <leo@the-earthworks.com>
2017-09-26 16:01:49 +0100

    Ajax Module wiring in progress for PUBSUB

---
+++ PubSubJS/Ajax/AjaxModule.js
@@ -0,0 +1,21 @@
+/**
+* Module : _AjaxModule
+* Public Api : _AjaxModule.init();
+* Created on : 26.09.2017
+* Author : Leo Jacobs
+*/
+ 
+var _AjaxModule = (function(window){
+'use strict';
+  var _PrivateSettings = {
+     $endPoint : 'https://api.github.com/users'
+  }
+ 
+  var init = function(){
+ 
+  }
+ 
+  return {
+    init: init
+  }
+}(window));
