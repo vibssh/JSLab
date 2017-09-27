edbe9123647672d27895b5a635fec3ea8cad08c6
Leo Jacobs <vibs.sh@gmail.com>
2017-09-27 22:32:09 +0100

    Wired PubSub Ajax Method

--- PubSubJS/Ajax/AjaxModule.js
+++ PubSubJS/Ajax/AjaxModule.js
@@ -8,14 +8,40 @@
 var _AjaxModule = (function(window){
 'use strict';
   var _PrivateSettings = {
-     $endPoint : 'https://api.github.com/users'
+     $endPoint : 'https://api.github.com/users',
+     $getUserBtn: $('#getUsers')
   }
  
-  var init = function(){
+  var bindUIActions = function(){
+    _PrivateSettings.$getUserBtn.on('click', function(e){
+      e.preventDefault();
+      _FetchData('GET', {}).done(_successMethod).fail(_errorMethod);
+    });
+  }
+
+  var _FetchData = function(method, args){
+    var args = args || {};
+    return $.ajax({
+      url: _PrivateSettings.$endPoint,
+      type: method,
+      dataType: args.$dataType || 'json',
+      contentType: 'application/json; charset=utf-8;',
+      data : args.$data || null || undefined
+    })
+  }
+
  
+  var _successMethod = function(data){
+    PubSub.publish(_EventManagement.evtSettings.Topics[0], data);
+  }
+
+  var _errorMethod = function(xhr){
+    PubSub.publish(_EventManagement.evtSettings.Topics[1], xhr);
   }
  
   return {
-    init: init
+    init: bindUIActions,
+    success: _successMethod,
+    failure: _errorMethod
   }
 }(window));
